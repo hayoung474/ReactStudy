ccb88b499926dfca75e96718561157a76be86cd2
hayoungzzzzz <hayoung474@gmail.com>
2021-02-26 21:25:20 +0900

    6. 조건부 렌더링 - 조건부 렌더링은 삼항연산자 또는 단축평가논리연산자 등으로 구현 가능 - 다양한 조건부 렌더링 문법이 존재함. 이는 코딩애플 참고

--- fastcampus-react/src/App.js
+++ fastcampus-react/src/App.js
@@ -1,14 +1,13 @@
-
-import React from 'react'
-import Hello from './Hello'
-import Wrapper from './Wrapper' // 이 방법 말고도 있음. 자동완성을 통해 가능
+import React from "react";
+import Hello from "./Hello";
+import Wrapper from "./Wrapper"; // 이 방법 말고도 있음. 자동완성을 통해 가능
 
 function App() {
   return (
     <Wrapper>
-    
-      <Hello name="react" color="red"/> {/* hello component */}
-      <Hello color="green"/>
+      <Hello name="react" color="red" isSpecial={true} />{" "}
+      {/* hello component, isSpecial={true} 와 그냥 isSpecial 만 쓰는 것 두 개는 동일한 효과. */}
+      <Hello color="green" />
     </Wrapper>
   );
 }
--- fastcampus-react/src/Hello.js
+++ fastcampus-react/src/Hello.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
 
 // camelCase naming
-// 구조분해할당으로 가능 
-function Hello({color,name}){
-    // JSX 
-    return(
-        // 스타일이던 뭐던 중괄호로 한번 감싸줘야 함..... Vue랑 다르다 이말이야
-        <div style={{color:color}}>안녕하세요 {name}!</div>
-    )
-}   
+// 구조분해할당으로 가능
+function Hello({ color, name, isSpecial }) {
+  // JSX
+  return (
+    // 스타일이던 뭐던 중괄호로 한번 감싸줘야 함..... Vue랑 다르다 이말이야
+    <div style={{ color: color }}>
+      {/*단순히 내용을 보여줌/숨김 여부를 결정할때는 && , 조건에 따라 내용이 달라져야 한다면 삼항연산자 추천*/}
+      {
+        isSpecial && (
+          <b>*</b>
+        ) /*삼항연산자를 이용한 조건부 렌더링 && 연산자가 더 편함. 단축평가논리연산자임.*/
+      }
+      안녕하세요 {name}!
+    </div>
+  );
+}
 
 // default props 설정
 Hello.defaultProps = {
-    name:"이름없음"
-}
+  name: "이름없음",
+};
 export default Hello;
